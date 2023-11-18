826a222e6efb20a0311f0cfa066a4670c4ad9de1
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-11-18 17:38:07 +0500

    fixed

---
+++ BuildingMaterials.Domain/Entities/Purchase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BuildingMaterials.Domain.Entities;
+
+public class Purchase
+{
+    public int Id { get; set; }
+    public int ClientId { get; set; }
+    public int MaterialId { get; set; }
+    public DateTime Date { get; set; } = DateTime.Now;
+    [ForeignKey(nameof(MaterialId))]
+    public Material Materials { get; set; }
+    [ForeignKey(nameof(ClientId))]
+    public Client Clients { get; set; }
+}
