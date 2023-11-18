00dc044b44d389a58e7a8dc85e0b92178e12334e
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-11-18 10:12:56 +0500

    Add project files.

---
+++ BuildingMaterilalsAPI/Controllers/BuildingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BuildingMaterilalsAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BuildingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Builing Controller");
+        }
+    }
+}
