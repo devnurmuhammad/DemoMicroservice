9c85fcd19db1fc4ecb05b657da555bc02b2c8dc9
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-11-19 19:53:16 +0500

    Properties added

---
+++ BuildingMaterilalsAPI/Repositories/Materials/IMaterialRepository.cs
@@ -0,0 +1,9 @@
+﻿using BuildingMaterials.Domain.Entities;
+using BuildingMaterials.Domain.Interfaces;
+
+namespace BuildingMaterilals.Web.Repositories.Materials
+{
+    public interface IMaterialRepository : IBaseRepository<Material>
+    {
+    }
+}
