8d678cd601777ddafa26a5d6d3be23156fde080b
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-11-19 21:46:53 +0500

    CreateMaterialController added

--- BuildingMaterilalsAPI/Controllers/BuildingController.cs
+++ BuildingMaterilalsAPI/Controllers/BuildingController.cs
@@ -1,4 +1,5 @@
-﻿using BuildingMaterilals.Web.Repositories.Materials;
+﻿using BuildingMaterilals.Web.DTOs;
+using BuildingMaterilals.Web.Repositories.Materials;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuildingMaterilalsAPI.Controllers
@@ -13,11 +14,20 @@ namespace BuildingMaterilalsAPI.Controllers
         {
             this.materialRepository = _materialRepository;
         }
+
         [HttpGet]
-        public IActionResult Get()
+        public async ValueTask<IActionResult> GetAllMaterialAsync()
         {
-            var result = materialRepository.GetAllAsync();
+            var result = await materialRepository.GetAllAsync();
             return Ok(result);
         }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateMaterialAsync(MaterialDTO materialDTO)
+        {
+            var result = await materialRepository.CreateAsync(materialDTO);
+            return Ok(result);
+        }
+
     }
 }
