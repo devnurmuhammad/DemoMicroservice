3656e170a9e663d081936826fade81d6d75d4960
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-11-19 19:51:38 +0500

    fixed

--- BuidingMaterials.Infrastructure/Data/ApplicationDbContext.cs
+++ BuidingMaterials.Infrastructure/Data/ApplicationDbContext.cs
@@ -10,5 +10,12 @@ namespace BuildingMaterials
         public DbSet<Material> Materials { get; set; }
         public DbSet<Client> Clients { get; set; }
         public DbSet<Purchase> Purchases { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Material>()
+                .HasIndex(x => x.Name)
+                .IsUnique();
+        }
     }
 }
