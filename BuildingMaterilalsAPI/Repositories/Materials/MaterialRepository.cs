256d11e26b54d5d99ccefbf15d331bfecb18fc2b
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-11-21 12:09:28 +0500

    fixed

--- BuildingMaterilalsAPI/Repositories/Materials/MaterialRepository.cs
+++ BuildingMaterilalsAPI/Repositories/Materials/MaterialRepository.cs
@@ -1,5 +1,4 @@
-﻿using BuildingMaterials;
-using BuildingMaterials.DataAccess.DataAccess;
+﻿using BuildingMaterials.DataAccess.DataAccess;
 using BuildingMaterials.Domain.Entities;
 using BuildingMaterilals.Web.DTOs;
 using Microsoft.EntityFrameworkCore;
@@ -16,41 +15,31 @@ namespace BuildingMaterilals.Web.Repositories.Materials
 
         public async ValueTask<bool> CreateAsync(MaterialDTO materialDTO)
         {
-                Material material = new Material();
-                material.Name = materialDTO.Name;
-                material.Description = materialDTO.Description;
-                material.Cost = materialDTO.Cost;
+            Material material = new Material();
+            material.Name = materialDTO.Name;
+            material.Description = materialDTO.Description;
+            material.Cost = materialDTO.Cost;
 
-                var result = await dbContext.AddAsync(material);
-                await dbContext.SaveChangesAsync();
+            var result = await dbContext.AddAsync(material);
+            await dbContext.SaveChangesAsync();
 
-                if (result != null)
-                {
-                    return true;
-                }
-                return false;
+            if (result != null)
+            {
+                return true;
+            }
+            return false;
         }
 
-        //public async ValueTask<bool> CreateAsync(Material model)
-        //{
-        //    Material material = new Material();
-        //    material.Name = model.Name;
-        //    material.Description = model.Description;
-        //    material.Cost = model.Cost;
-
-        //    var result = await dbContext.AddAsync(material);
-        //    await dbContext.SaveChangesAsync();
-
-        //    if (result != null)
-        //    {
-        //        return true;
-        //    }
-        //    return false;
-        //}
-
-        public ValueTask<bool> DeleteAsync(string name)
+        public async ValueTask<bool> DeleteAsync(string name)
         {
-            throw new NotImplementedException();
+            var result = await dbContext.Materials.FirstOrDefaultAsync(x => x.Name == name);
+            if (result != null)
+            {
+                dbContext.Remove(result);
+                await dbContext.SaveChangesAsync();
+                return true;
+            }
+            return false;
         }
 
         public async ValueTask<IList<Material>> GetAllAsync()
@@ -59,19 +48,30 @@ namespace BuildingMaterilals.Web.Repositories.Materials
             return result;
         }
 
-        public ValueTask<Material> GetByNameAsync(string name)
+        public async ValueTask<Material> GetByNameAsync(string name)
         {
-            throw new NotImplementedException();
+            Material? result = await dbContext.Materials.FirstOrDefaultAsync(x => x.Name == name);
+            return result != null ? result : new Material();
         }
 
-        public ValueTask<int> GetCountAsync()
+        public async ValueTask<int> GetCountAsync()
         {
-            throw new NotImplementedException();
+            var result = await dbContext.Materials.CountAsync();
+            return result;
         }
 
-        public ValueTask<bool> UpdateAsync(string name, Material model)
+        public async ValueTask<bool> UpdateAsync(string name, MaterialDTO modelDTO)
         {
-            throw new NotImplementedException();
+            var result = await dbContext.Materials.FirstOrDefaultAsync(x => x.Name == name);
+            if (result != null)
+            {
+                //result.Name = modelDTO.Name;
+                //result.Description = modelDTO.Description;
+                result.Cost = modelDTO.Cost;
+                await dbContext.SaveChangesAsync();
+                return true;
+            }
+            return false;
         }
     }
 }
