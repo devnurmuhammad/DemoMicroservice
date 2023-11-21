1578171c8114c3756729c4fd3473479efc2d8b75
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-11-21 12:09:39 +0500

    fixed

--- BuildingMaterilalsAPI/Repositories/IBaseRepository.cs
+++ BuildingMaterilalsAPI/Repositories/IBaseRepository.cs
@@ -5,7 +5,7 @@ namespace BuildingMaterials.Domain.Interfaces;
 public interface IBaseRepository<TModel> where TModel : class
 {
     public ValueTask<bool> CreateAsync(MaterialDTO materialDTO);
-    public ValueTask<bool> UpdateAsync(string name, TModel model);
+    public ValueTask<bool> UpdateAsync(string name, MaterialDTO materialDTO);
     public ValueTask<bool> DeleteAsync(string name);
     public ValueTask<IList<TModel>> GetAllAsync();
     public ValueTask<TModel> GetByNameAsync(string name);
